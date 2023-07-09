b47d08ba27b29eb10a14c518547d4609e06470a4
Yunfan Zhang <138235459+OrangeFender@users.noreply.github.com>
2023-07-09 14:53:14 +0800

    Update RFC6962.c

--- Project5/RFC6962.c
+++ Project5/RFC6962.c
@@ -1,10 +1,22 @@
-typedef struct{
-    int N;
-    char detial[200];
+struct Node;
+typedef struct transection{
+    char detial[1024];//假设交易信息为1024字节
+    Node* father;
 }transection;
 
-int biuild(int amount,transection* trans){
+typedef struct Node{
+Node* father;
+Node*lchild;
+Node*rchild;
+transection*lleaf;
+transection*rleaf;
+}Node;
+
+int build(int amount,transection* trans){
 int depth;
 for(depth=0;(amount>>depth)>0;depth++);
 
+while(depth>0){
+        
+}
 }

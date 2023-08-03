4e986368dc7f6cadecf9356ebcf9e7d4b89a2d63
Yunfan Zhang <zyfdmap@126.com>
2023-08-03 11:40:11 +0800

    Prj4 finished

--- Project4/SM3/Expand.c
+++ Project4/SM3/Expand.c
@@ -1,62 +1,122 @@
 ﻿typedef unsigned int UINT32;
 #include <immintrin.h>
-#include<string.h>
+
+
+#define LeftRotate(word, bits)( (word) << (bits) | (word) >> (32 - (bits)) )
+#define SIMD
+
+__m128i rotate_left_epi32(__m128i a, int bits) {
+    __m128i leftShifted = _mm_slli_epi32(a, bits);
+    __m128i rightShifted = _mm_srli_epi32(a, 32 - bits);
+    return _mm_xor_si128(leftShifted, rightShifted);
+}
+
 void Expand(UINT32 B[16], UINT32 W[68 + 3], UINT32 Ww[64]) {//W长度为68但是需要加三以确保足够SIMD寄存器存放
-
     {
-        memcpy(W, B, 64);
+
+        memcpy(W, B, 16*4);
         
     }
-    register __m128i va, vb, vc, vd, ve, vl, vr; 
-    //register __m128i va, vb;
-#pragma loop(hint_parallel(17))
-    for (int j = 16; j < 68; j += 3) {
 
-    //    va = _mm_xor_si128(_mm_srli_epi32(*((__m128i*)W + j - 3), 32 - 15) , _mm_slli_epi32(*((__m128i*)W + j - 3), 15));
-    //    vb = _mm_xor_si128(*((__m128i*)W + j - 9), *((__m128i*)W + j - 16));
-    //    va = _mm_xor_si128(va, vb);
+#ifdef SIMD
+    /*
 
-    //    vb = _mm_xor_si128(_mm_slli_epi32(va, 15),_mm_srli_epi32(va, 32 - 15));
-    //    vb = _mm_xor_si128(vb, _mm_srli_epi32(va, 32 - 23));
-    //    va = _mm_xor_si128(va, _mm_srli_epi32(va, 32 - 23));
-    //    va = _mm_xor_si128(va,vb);
-    //    vb = _mm_xor_si128(_mm_slli_epi32(*((__m128i*)W + j - 13), 7), _mm_srli_epi32(*((__m128i*)W + j - 13), 32 - 7));
-    //    va = _mm_xor_si128(va, *((__m128i*)W + j - 6));
-    //     vb= _mm_xor_si128(va,vb);
-    //     *(__m128i*)(W + j) = vb;
+    //register __m256i w16, w8, w9, w13;
+    register __m256i  w13,tmp;
+#pragma loop(hint_parallel(7))
+
+    for (int j = 16; j < 68; j += 8) {
+        //w16 = _mm256_load_si256((__m256i*)(W + j - 16));
+        //w9 = _mm256_loadu_si256((__m256i*)(W + j - 9));
+        w13 = _mm256_loadu_si256((__m256i*)(W + j - 13));
+        //register __m256i tmpv = _mm256_xor_si256(w16, w9);
+        //_mm256_store_si256((__m256i*)(W + j), _mm256_xor_si256(
+        //    _mm256_xor_si256(_mm256_xor_si256(_mm256_slli_epi32(tmpv, 15), _mm256_srli_epi32(tmpv, 32 - 15)), _mm256_xor_si256(_mm256_slli_epi32(tmpv, 23), _mm256_srli_epi32(tmpv, 32 - 23)))
+        //    , _mm256_xor_si256(tmpv, _mm256_xor_si256(_mm256_slli_epi32(w13, 7), _mm256_srli_epi32(w13, 32 - 7)))));
+        _mm256_store_si256((__m256i*)(W + j), _mm256_xor_si256(_mm256_slli_epi32(w13, 7), _mm256_srli_epi32(w13, 32 - 7)));
+        //tmp = _mm256_xor_si256(_mm256_slli_epi32(w13, 7), _mm256_srli_epi32(w13, 32 - 7));
+
+
+#pragma loop(hint_parallel(8))
+        for (int i = 0; i < 8; i++) {
+            UINT32 a = LeftRotate(W[j + i - 3], 15)^ W[j + i - 16]^W[j + i - 9];
+            UINT32 b = LeftRotate(a, 15) ^ LeftRotate(a, 23) ^ a;
+            W[j + i] ^= (W[j + i - 6] ^ b);
+            //printf("%x ", W[j + i]);
+        }//printf("\n");
+    }
+    */
+    
+    __m128i va, vb, vc, vd, ve;
+    for (int j = 16; j < 68; j += 4) {
         
-        va = _mm_loadu_si128((__m128i*)(W + j - 16));
+        va = _mm_load_si128((__m128i*)(W + j - 16));
         vb = _mm_loadu_si128((__m128i*)(W + j - 9));
-        vc = _mm_loadu_si128((__m128i*)(W + j - 3));
+        //vc = _mm_loadu_si128((__m128i*)(W + j - 3));
+        vc = _mm_set_epi32(0,W[j - 3+2], W[j - 3 + 1], W[j - 3]);
         vd = _mm_loadu_si128((__m128i*)(W + j - 13));
-        vl = _mm_slli_epi32(vc, 15);
-        vr = _mm_srli_epi32(vc, 32 - 15);
-        va = _mm_xor_si128(va, vl);
-        vr = _mm_xor_si128(vr, vb);//(vc<<<15)⊕va⊕vb
-        ve = _mm_load_si128((__m128i*)(W + j - 6));
-        vc = _mm_xor_si128(va, vr);//vc=Wj−16 ⊕ Wj−9 ⊕ (Wj−3 <<< 15)
-        vl = _mm_slli_epi32(vc, 15);
-        vr = _mm_srli_epi32(vc, 32 - 15);
-        va = _mm_slli_epi32(vc, 23);
-        vl = _mm_xor_si128(vl, vr);
-        vb = _mm_srli_epi32(vc, 32 - 23);
-        vc = _mm_xor_si128(vc, vl);
-        va = _mm_xor_si128(vb, va);
-        vl = _mm_slli_epi32(vd, 7);
-        vc = _mm_xor_si128(va, vc);
-        vr = _mm_srli_epi32(vd, 32 - 7);
-        vc = _mm_xor_si128(vc, vl);
-        ve = _mm_xor_si128(ve, vr);
-        vc = _mm_xor_si128(ve, vc);
-        _mm_storeu_si128((__m128i*)(W + j), vc);
+        ve = _mm_loadu_si128((__m128i*)(W + j - 6));
+        __m128i tmp1 = _mm_xor_si128(_mm_xor_si128(va, vb), rotate_left_epi32(vc, 15));
+        __m128i tmp2 = _mm_xor_si128(_mm_xor_si128(rotate_left_epi32(tmp1, 15), rotate_left_epi32(tmp1, 23)),tmp1);
+        __m128i tmp3 = _mm_xor_si128(tmp2, _mm_xor_si128(rotate_left_epi32(vd, 7), ve));
+        _mm_store_si128((__m128i*)(W + j), tmp3);
+      
+        UINT32 t = LeftRotate(W[j], 15);
+        W[j + 3] = W[j+3] ^ t ^ LeftRotate(t, 15) ^ LeftRotate(t, 23);
         
     }
+    
+    //__m128i X, T, R;
+    //__m128i M = _mm_setr_epi32(0, 0, 0, 0xffffffff);
+    //for (int j = 16; j < 68; j += 4) {
+    //    /* X = (W[j - 3], W[j - 2], W[j - 1], 0) */
+    //    X = _mm_loadu_si128((__m128i*)(W + j - 3));
+    //    X = _mm_andnot_si128(M, X);
+
+    //    X = rotate_left_epi32(X, 15);
+    //    T = _mm_loadu_si128((__m128i*)(W + j - 9));
+    //    X = _mm_xor_si128(X, T);
+    //    T = _mm_loadu_si128((__m128i*)(W + j - 16));
+    //    X = _mm_xor_si128(X, T);
+
+    //    /* P1() */
+    //    T = rotate_left_epi32(X, (23 - 15));
+    //    T = _mm_xor_si128(T, X);
+    //    T = rotate_left_epi32(T, 15);
+    //    X = _mm_xor_si128(X, T);
+
+    //    T = _mm_loadu_si128((__m128i*)(W + j - 13));
+    //    T = rotate_left_epi32(T, 7);
+    //    X = _mm_xor_si128(X, T);
+    //    T = _mm_loadu_si128((__m128i*)(W + j - 6));
+    //    X = _mm_xor_si128(X, T);
+
+    //    /* W[j + 3] ^= P1(ROL32(W[j + 1], 15)) */
+    //    R = _mm_shuffle_epi32(X, 0);
+    //    R = _mm_and_si128(R, M);
+    //    T = rotate_left_epi32(R, 15);
+    //    T = _mm_xor_si128(T, R);
+    //    T = rotate_left_epi32(T, 9);
+    //    R = _mm_xor_si128(R, T);
+    //    R = rotate_left_epi32(R, 6);
+    //    X = _mm_xor_si128(X, R);
+
+    //    _mm_storeu_si128((__m128i*)(W + j), X);
+
+
+#else   
+    for (int j = 16; j < 68; j ++) {
+            UINT32 tmp = W[j - 16] ^ W[j - 9] ^ LeftRotate(W[j - 3], 15);
+            W[j] = LeftRotate(tmp, 15) ^ LeftRotate(tmp, 23) ^ tmp ^ W[j - 6] ^ LeftRotate(W[j - 13], 7);
+        }
+#endif
+       
     {
-        __m256i w1, w2;
+        register __m256i w1, w2;
         for (int i = 0; i < 64; i += 8)
         {
-            w1 = _mm256_load_si256(W + i);
-            w2 = _mm256_load_si256(W + i + 4);
+            w1 = _mm256_loadu_si256(W + i);
+            w2 = _mm256_loadu_si256(W + i + 4);
             w1 = _mm256_xor_si256(w1, w2);
             _mm256_store_si256((__m256*)(Ww + i), w1);
         }
--- Project4/SM3/SM3.c
+++ Project4/SM3/SM3.c
@@ -6,56 +6,191 @@
 #define LeftRotate(word, bits)( (word) << (bits) | (word) >> (32 - (bits)) )
 
 #define p0(x) (x^LeftRotate(x,9)^LeftRotate(x,17))
-#define T1 0x79cc4519
-#define T2 0x7a879d8a
+
+typedef unsigned int UINT32;
+
+
+const UINT32 Tj[64] = { 
+    0x79cc4519U, 0xf3988a32U, 0xe7311465U, 0xce6228cbU,
+    0x9cc45197U, 0x3988a32fU, 0x7311465eU, 0xe6228cbcU,
+    0xcc451979U, 0x988a32f3U, 0x311465e7U, 0x6228cbceU,
+    0xc451979cU, 0x88a32f39U, 0x11465e73U, 0x228cbce6U,
+    0x9d8a7a87U, 0x3b14f50fU, 0x7629ea1eU, 0xec53d43cU,
+    0xd8a7a879U, 0xb14f50f3U, 0x629ea1e7U, 0xc53d43ceU,
+    0x8a7a879dU, 0x14f50f3bU, 0x29ea1e76U, 0x53d43cecU,
+    0xa7a879d8U, 0x4f50f3b1U, 0x9ea1e762U, 0x3d43cec5U,
+    0x7a879d8aU, 0xf50f3b14U, 0xea1e7629U, 0xd43cec53U,
+    0xa879d8a7U, 0x50f3b14fU, 0xa1e7629eU, 0x43cec53dU,
+    0x879d8a7aU, 0x0f3b14f5U, 0x1e7629eaU, 0x3cec53d4U,
+    0x79d8a7a8U, 0xf3b14f50U, 0xe7629ea1U, 0xcec53d43U,
+    0x9d8a7a87U, 0x3b14f50fU, 0x7629ea1eU, 0xec53d43cU,
+    0xd8a7a879U, 0xb14f50f3U, 0x629ea1e7U, 0xc53d43ceU,
+    0x8a7a879dU, 0x14f50f3bU, 0x29ea1e76U, 0x53d43cecU,
+    0xa7a879d8U, 0x4f50f3b1U, 0x9ea1e762U, 0x3d43cec5U,
+};
+
 #define FF1(x,y,z) (x^y^z)
 #define FF2(x,y,z) ((x&y)|(x&z)|(y&z))
 #define GG1(x,y,z) (x^y^z)
 #define GG2(x,y,z) ((x&y)|((~x)&z))
 
-typedef unsigned int UINT32;
-
-void Expand(UINT32 B[16], UINT32 W[68 + 3], UINT32 Ww[64]);
-void compress(UINT32 V[8],UINT32 W[68], UINT32 Ww[64]){
-UINT32 A,B,C,D,E,F,G,H,SS1,SS2,TT1,TT2;
-A=V[0];B=V[1];C=V[2];D=V[3];E=V[4];F=V[5];G=V[6];H=V[7];
-#pragma loop(hint_parallel(16))
-for(int j=0;j<16;j++){
-SS1=LeftRotate((LeftRotate(A,12)+E+LeftRotate(T1,j)),7);
-SS2=SS1^LeftRotate(A,12);
-TT1=FF1(A,B,C)+D+SS2+Ww[j];
-TT2=GG1(E,F,G)+H+SS1+W[j];
-D=C;
-C=LeftRotate(B,9);
-B=A;
-A=TT1;
-H=G;
-G=LeftRotate(F,19);
-F=E;
-E=p0(TT2);
-
-}
-#pragma loop(hint_parallel(48))
-for(int j=16;j<64;j++){
-SS1=LeftRotate((LeftRotate(A,12)+E+LeftRotate(T2,j)),7);
-SS2=SS1^LeftRotate(A,12);
-TT1=FF2(A,B,C)+D+SS2+Ww[j];
-TT2=GG2(E,F,G)+H+SS1+W[j];
-D=C;
-C=LeftRotate(B,9);
-B=A;
-A=TT1;
-H=G;
-G=LeftRotate(F,19);
-F=E;
-E=p0(TT2);
-
-}
-V[0]^=A;V[1]^=B;V[2]^=C;V[3]^=D;V[4]^=E;V[5]^=F;V[6]^=G;V[7]^=H;
-
-
+__m128i rotate_left_epi32(__m128i a, int bits) {
+    __m128i leftShifted = _mm_slli_epi32(a, bits);
+    __m128i rightShifted = _mm_srli_epi32(a, 32 - bits);
+    return _mm_xor_si128(leftShifted, rightShifted);
 }
 
+void round(UINT32 b[16], UINT32 W[68 + 3], UINT32 V[8]) {
+    memcpy(W, b, 16 * 4);
+   
+
+    UINT32 A, B, C, D, E, F, G, H, SS1, SS2, TT1, TT2;
+    A = V[0]; B = V[1]; C = V[2]; D = V[3]; E = V[4]; F = V[5]; G = V[6]; H = V[7];
+
+    /*
+    for (int j = 16; j < 32; j += 1) {
+        UINT32 tmp = W[j - 16] ^ W[j - 9] ^ LeftRotate(W[j - 3], 15);
+        W[j] = LeftRotate(tmp, 15) ^ LeftRotate(tmp, 23) ^ tmp ^ W[j - 6] ^ LeftRotate(W[j - 13], 7);
+
+        SS1 = LeftRotate((LeftRotate(A, 12) + E + Tj[j - 16]), 7);
+        SS2 = SS1 ^ LeftRotate(A, 12);
+        TT1 = FF1(A, B, C) + D + SS2 + (W[j - 16] ^ W[j - 16 + 4]);
+        TT2 = GG1(E, F, G) + H + SS1 + W[j - 16];
+        D = C;
+        C = LeftRotate(B, 9);
+        B = A;
+        A = TT1;
+        H = G;
+        G = LeftRotate(F, 19);
+        F = E;
+        E = p0(TT2);
+    }
+    for (int j = 32; j < 68; j += 1) {
+        UINT32 tmp = W[j - 16] ^ W[j - 9] ^ LeftRotate(W[j - 3], 15);
+        W[j] = LeftRotate(tmp, 15) ^ LeftRotate(tmp, 23) ^ tmp ^ W[j - 6] ^ LeftRotate(W[j - 13], 7);
+
+        SS1 = LeftRotate((LeftRotate(A, 12) + E + Tj[j - 16]), 7);
+        SS2 = SS1 ^ LeftRotate(A, 12);
+        //printf("W:%x,%x\n", W[j - 16 + i], W[j - 16 + i + 4]);
+        TT1 = FF2(A, B, C) + D + SS2 + (W[j - 16] ^ W[j - 16 + 4]);
+        TT2 = GG2(E, F, G) + H + SS1 + W[j - 16];
+        D = C;
+        C = LeftRotate(B, 9);
+        B = A;
+        A = TT1;
+        H = G;
+        G = LeftRotate(F, 19);
+        F = E;
+        E = p0(TT2);
+    }
+    for (int j = 68 - 16; (j) < 64; j++) {
+        SS1 = LeftRotate((LeftRotate(A, 12) + E + Tj[j]), 7);
+        SS2 = SS1 ^ LeftRotate(A, 12);
+        TT1 = FF2(A, B, C) + D + SS2 + (W[j] ^ W[j + 4]);
+        TT2 = GG2(E, F, G) + H + SS1 + W[j];
+        D = C;
+        C = LeftRotate(B, 9);
+        B = A;
+        A = TT1;
+        H = G;
+        G = LeftRotate(F, 19);
+        F = E;
+        E = p0(TT2);
+        //printf("%d A:%x B:%x C:%x D:%x E:%x F:%x G:%x H:%x \n",j, A, B, C, D, E, F, G, H);
+    }
+    */
+    
+    __m128i va, vb, vc, vd, ve;
+    const __m128i vmask = _mm_set_epi32(0, 0xffffffff, 0xffffffff, 0xffffffff);
+#pragma loop(hint_parallel(4))
+    for (int j = 16; j < 32; j += 4) {
+        vc = _mm_loadu_si128((__m128i*)(W + j - 3));
+        va = _mm_load_si128((__m128i*)(W + j - 16));
+        vb = _mm_loadu_si128((__m128i*)(W + j - 9));
+        //vc = _mm_setr_epi32(0, W[j - 3 + 2], W[j - 3 + 1], W[j - 3]);
+        vd = _mm_loadu_si128((__m128i*)(W + j - 13));
+        vc = _mm_and_si128(vc, vmask);
+        ve = _mm_loadu_si128((__m128i*)(W + j - 6));
+
+        __m128i tmp1 = _mm_xor_si128(_mm_xor_si128(va, vb), rotate_left_epi32(vc, 15));
+        __m128i tmp2 = _mm_xor_si128(_mm_xor_si128(rotate_left_epi32(tmp1, 15), rotate_left_epi32(tmp1, 23)), tmp1);
+        __m128i tmp3 = _mm_xor_si128(tmp2, _mm_xor_si128(rotate_left_epi32(vd, 7), ve));
+        _mm_store_si128((__m128i*)(W + j), tmp3);
+        UINT32 t = LeftRotate(W[j], 15);
+        W[j + 3] = W[j + 3] ^ t ^ LeftRotate(t, 15) ^ LeftRotate(t, 23);
+
+#pragma loop(hint_parallel(4))
+        for (int i = 0; i < 4; i++) {
+            SS1 = LeftRotate((LeftRotate(A, 12) + E + Tj[j-16+i]), 7);
+            SS2 = SS1 ^ LeftRotate(A, 12);
+            TT1 = FF1(A, B, C) + D + SS2 + (W[j-16+i]^ W[j - 16 + i+4]);
+            TT2 = GG1(E, F, G) + H + SS1 + W[j-16+i];
+            D = C;
+            C = LeftRotate(B, 9);
+            B = A;
+            A = TT1;
+            H = G;
+            G = LeftRotate(F, 19);
+            F = E;
+            E = p0(TT2);
+            //printf("%d A:%x B:%x C:%x D:%x E:%x F:%x G:%x H:%x \n",j-16+i, A, B, C, D, E, F, G, H);
+        }
+    }
+#pragma loop(hint_parallel(9))
+    for (int j = 32; j < 68; j += 4) {
+        vc = _mm_loadu_si128((__m128i*)(W + j - 3));
+        va = _mm_load_si128((__m128i*)(W + j - 16));
+        vb = _mm_loadu_si128((__m128i*)(W + j - 9));
+        //vc = _mm_setr_epi32(0, W[j - 3 + 2], W[j - 3 + 1], W[j - 3]);
+        vd = _mm_loadu_si128((__m128i*)(W + j - 13));
+        vc = _mm_and_si128(vc, vmask);
+        ve = _mm_loadu_si128((__m128i*)(W + j - 6));
+        __m128i tmp1 = _mm_xor_si128(_mm_xor_si128(va, vb), rotate_left_epi32(vc, 15));
+        __m128i tmp2 = _mm_xor_si128(_mm_xor_si128(rotate_left_epi32(tmp1, 15), rotate_left_epi32(tmp1, 23)), tmp1);
+        __m128i tmp3 = _mm_xor_si128(tmp2, _mm_xor_si128(rotate_left_epi32(vd, 7), ve));
+        _mm_store_si128((__m128i*)(W + j), tmp3);
+
+        UINT32 t = LeftRotate(W[j], 15);
+        W[j + 3] = W[j + 3] ^ t ^ LeftRotate(t, 15) ^ LeftRotate(t, 23);
+
+#pragma loop(hint_parallel(4))
+        for (int i = 0; i < 4; i++) {
+            int ind = j - 16 + i;
+            SS1 = LeftRotate((LeftRotate(A, 12) + E + Tj[j-16+i]), 7);
+            SS2 = SS1 ^ LeftRotate(A, 12);
+            //printf("W:%x,%x\n", W[j - 16 + i], W[j - 16 + i + 4]);
+            TT1 = FF2(A, B, C) + D + SS2 + (W[j - 16 + i] ^ W[j - 16 + i + 4]);
+            TT2 = GG2(E, F, G) + H + SS1 + W[j-16+i];
+            D = C;
+            C = LeftRotate(B, 9);
+            B = A;
+            A = TT1;
+            H = G;
+            G = LeftRotate(F, 19);
+            F = E;
+            E = p0(TT2);
+            //printf("%d A:%x B:%x C:%x D:%x E:%x F:%x G:%x H:%x \n", j - 16 + i, A, B, C, D, E, F, G, H);
+        }
+    }
+#pragma loop(hint_parallel(12))
+    for (int j = 68-16; (j) < 64; j++) {
+        SS1 = LeftRotate((LeftRotate(A, 12) + E + Tj[j]), 7);
+        SS2 = SS1 ^ LeftRotate(A, 12);
+        TT1 = FF2(A, B, C) + D + SS2 +( W[j] ^ W[j + 4]);
+        TT2 = GG2(E, F, G) + H + SS1 + W[j];
+        D = C;
+        C = LeftRotate(B, 9);
+        B = A;
+        A = TT1;
+        H = G;
+        G = LeftRotate(F, 19);
+        F = E;
+        E = p0(TT2);
+        //printf("%d A:%x B:%x C:%x D:%x E:%x F:%x G:%x H:%x \n",j, A, B, C, D, E, F, G, H);
+    }
+    
+    V[0] ^= A; V[1] ^= B; V[2] ^= C; V[3] ^= D; V[4] ^= E; V[5] ^= F; V[6] ^= G; V[7] ^= H;
+}
 int SM3(int length, unsigned char* m, unsigned char* hash) {
     UINT32* V = hash;
     const __m256i ShuffleRev = _mm256_set_epi8(
@@ -66,17 +201,19 @@ int SM3(int length, unsigned char* m, unsigned char* hash) {
     int n=((length+1+8+63)/64);
     unsigned long long l=64*n;//填充后长度为64字节的倍数(分组为512bit)
     unsigned char*M=(unsigned char*)malloc(l);
+    /*
     for (int i = 0; i < l; i++) {
         M[i] = 0;
-    }
-    memset(M,0,l);
-
-    /*for(int i=0;i<length;i++){
-        M[i]=m[i];
     }*/
+    memset(M,0,l);
+    /*
+    for(int i=0;i<length;i++){
+        M[i]=m[i];
+    }
+    */
     memcpy(M, m, length);
     M[length]=0x80;
-
+    
     for (int i = 0; i < length + 1; i += 32) {
         __m256i b = _mm256_load_si256((__m256i*)(M + i));
          b = _mm256_shuffle_epi8(b, ShuffleRev);
@@ -85,12 +222,12 @@ int SM3(int length, unsigned char* m, unsigned char* hash) {
     long long lenbit = length * 8;
     *(unsigned int *)(&M[l-4])= (int)lenbit;//末尾填充长度，单位是bit
     *(unsigned int*)(&M[l - 8]) = lenbit>>32;
-    UINT32 W[68+3]; UINT32 Ww[64];
+    UINT32 W[68+7]; 
 
     for(int i=0;i<n;i++){
-        Expand((UINT32*)(M+i*64),W,Ww);
-        compress(V,W,Ww);
+        round((UINT32*)(M + i * 64), W, V);
     }
+    
     __m256i b = _mm256_load_si256((__m256i*)V);
     b = _mm256_shuffle_epi8(b, ShuffleRev);
     _mm256_store_si256((__m256i*)V, b);
